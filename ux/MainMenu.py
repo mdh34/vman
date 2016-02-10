68362be89885ffcdc3196dd3f6a94e21cb65c9e7
Fernando Andrade <fernandofreamunde@gmail.com>
2016-02-11 00:52:44 +0100

    Fiz cenas -.- :)

---
+++ ux/MainMenu.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+#import important stuff
+import os
+import signal
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+
+class MainMenu():
+	ID = 'VmanMenu'
+	def __init__(self):
+		indicator = appindicator.Indicator.new(self.ID, os.path.abspath('sample_icon1.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+		indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+		indicator.set_menu(self.build_menu())
+		gtk.main()
+
+	def build_menu(self):
+		menu = gtk.Menu()
+
+		item_quit = gtk.MenuItem('Add Box')
+		item_quit.connect('activate', self.add_box)
+		menu.append(item_quit)
+
+		item_quit = gtk.MenuItem('Configuration')
+		item_quit.connect('activate', self.configuration)
+		menu.append(item_quit)
+
+		item_quit = gtk.MenuItem('About')
+		item_quit.connect('activate', self.about)
+		menu.append(item_quit)
+
+		#item_quit = gtk.MenuItem('Play/pause')
+		#item_quit.connect('activate', self.up_suspend)
+		#menu.append(item_quit)
+		#
+		#separator =  gtk.SeparatorMenuItem()
+		#menu.append(separator)
+
+		item_quit = gtk.MenuItem('Exit')
+		item_quit.connect('activate', self.quit)
+		menu.append(item_quit)
+
+		menu.show_all()
+		return menu
+
+	def add_box(self,s):
+		print "add_box"
+
+	def configuration(self,s):
+		print "configuration"
+
+	def about(self,s):
+		print "about"
+
+	def quit(self,s):
+		gtk.main_quit()
--- vman.py
+++ vman.py
@@ -3,33 +3,56 @@
 import os
 import signal
 from gi.repository import Gtk, Gio
+#import os.path
 
 #from ux.Window import Window
 from ux.Indicator import Indicator
+from ux.MainMenu import MainMenu
 from src.Box import Box
 
+class Vman():
+	"""docstring for Vman"""
+	def __init__(self):
+		#super(Vman, self).__init__()
+		#self.arg = arg
 
-"""
-reads the locations of the boxes,
-returns a list with the locations
-Will be replaced by vagrant global-status
-"""
-def readBoxes():
-	#file = open('data/boxes')
-	print "reading file..."
-	with open('data/boxes') as file:
-		content = file.readlines()
-	print "processing list of boxes..."
-	boxesList = []
-	for path in content:
-			boxesList.append(path[:-1])
-	print "boxes list is : "
-	print boxesList
-	return boxesList
-	print file.read()
+		#get home folder
+		home = os.path.expanduser("~")
+		#get if boxes file exists
+		boxes = os.path.isfile(home + '/.vman/boxes')
 
-readBoxes()
-#ind = Indicator()
+		if not boxes:
+			print "Needs Configs!"
+		else:
+			print "Does not need Config"
+
+		
+		"""
+		reads the locations of the boxes,
+		returns a list with the locations
+		Will be replaced by vagrant global-status
+		"""
+		def readBoxes(self):
+			file = open('data/boxes')
+			print "reading config file..."
+			with open('data/boxes') as file:
+				content = file.readlines()
+			print "processing list of boxes..."
+			boxesList = []
+			for path in content:
+					boxesList.append(path[:-1])
+			print "boxes list is : "
+			print boxesList
+			return boxesList
+			print file.read()
+
+		#readBoxes()
+
+app=Vman()
+		
+
+
+#ind = MainMenu()
 #win = Window()
 #
 #win.connect("delete-event", Gtk.main_quit)
