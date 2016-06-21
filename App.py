a964decc961a7235c8522bd8c8ea50c66bb9e010
Fernando Andrade <fernandofreamunde@gmail.com>
2016-06-21 22:27:19 +0200

    name changed

---
+++ App.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sqlite3 as lite
+from config.database import database
+from core.App import App
+from core.Controller import Controller
+
+App = App()
